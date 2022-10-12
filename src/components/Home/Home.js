4ba22b4b5414964bb724a90e8312865ab838546b
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-10-12 19:04:25 +0600

    responsive banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,14 +14,14 @@ const Home = () => {
         <div className="topics container lg:mx-auto">
             <br />
             <div className="container mx-auto">
-                <div className="carousel mx-auto w-96 lg:w-full rounded-3xl">
-                    <div id="item1" className="carousel-item w-96 lg:w-full ">
+                <div className="carousel mx-auto w-80 lg:w-full rounded-xl lg:rounded-3xl">
+                    <div id="item1" className="carousel-item w-80 lg:w-full ">
                         <img className='w-96 lg:w-full mx-auto h-full' src="https://i.ibb.co/DD4w4zp/quiz-Copy.jpg" alt="quiz-banner" border="0" />
                     </div>
                 </div>
             </div>
 
-            <div className="grid lg:grid-cols-4 grid-cols-1 lg:gap-20 gap-5 my-10 topic-container ">
+            <div className="grid lg:grid-cols-4 grid-cols-1 lg:gap-20 gap-5 my-5 lg:my-10 topic-container ">
                 {
                     data.map(quiz => <TopicCard key={quiz.id} quiz={quiz}></TopicCard>)
                 }
