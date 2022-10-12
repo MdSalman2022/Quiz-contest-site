8495e8c00ab30b0460d115d0480d42e88724adc1
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-10-12 16:41:16 +0600

    Error page added

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -10,10 +10,6 @@ export default function ErrorPage() {
             <h1 className="text-4xl my-10 font-bold">OPPS!PAGE NOT FOUND</h1>
             <p className="text-2xl my-10">Sorry, the page you're looking for doesn't exist</p>
             <Link to='/'> <button className="btn btn-block container mx-auto my-20">Return Home</button></Link>
-
-            {/* <p>
-                <i>{error.statusText || error.message}</i>
-            </p> */}
         </div>
     );
 }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -9,14 +9,14 @@ const Statistics = () => {
 
     return (
         <div className='flex align-center justify-center w-66 h-full'>
-            {/* <ResponsiveContainer width="100%" height="100%"> */}
-            <BarChart width={400} height={400} data={data}>
-                <Bar dataKey="total" fill="#2D213B" />
-                <XAxis dataKey="name" />
-                <YAxis dataKey="total" />
-                <Tooltip></Tooltip>
-            </BarChart>
-            {/* </ResponsiveContainer> */}
+            <ResponsiveContainer width="80%" height={600}>
+                <BarChart data={data}>
+                    <Bar dataKey="total" fill="#2D213B" />
+                    <XAxis dataKey="name" />
+                    <YAxis dataKey="total" />
+                    <Tooltip></Tooltip>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
